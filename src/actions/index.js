07bbc66e335df968696823fc6a05ba9005f2b74e
Yoseph Anderson <yrganderson@gmail.com>
2016-12-23 10:08:11 -0700

    secure api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${process.env.REACT_APP_API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${process.env.REACT_APP_API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
